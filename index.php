e942b4c284545ab241eac24f9efe4df9699777f0
Daniel.García <sanck.one@gmail.com>
2020-02-20 13:14:49 -0800

    Prueba de fondo css en base a platzi

--- index.php
+++ index.php
@@ -11,10 +11,29 @@
 </head>
 <body>
 
-<div class="jumbotron text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p> 
+<div class="jumbotron text-center" style="margin-bottom:0px; background: url(https://static.platzi.com/bff/image/bg_hero-833887b….png),linear-gradient(90deg,#1c3643,#273b47 25%,#1e5372);">
+    <h2 ><span style="color:white; text-align:right;">La escuela online de formación profesional en tecnología</span></h2>
+  
+    <div class="HeroContent-stats">
+    <div class="HeroContent-elements">
+    <div class="HeroContent-stats-item">
+    <span>70<i>%</i></span>
+    <span>de los graduados de Platzi <br> duplican sus ingresos</span>
+    </div>
+    <div class="HeroContent-stats-item">
+    <span>20<i>%</i></span>
+    <span>crean su propia empresa <br> de tecnología o startup</span>
+    </div>
+    <div class="HeroContent-image">
+    </div>
+    </div>
+    </div>
+
 </div>
+<div class="Diagonal"><svg style="margin-top: -58;
+    width: 100%;
+    height: 30px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 10" preserveAspectRatio="none" fill="white"><polygon points="100 0 100 10 0 10"></polygon></svg></div>
+
   
 <div class="container">
   <div class="row">
